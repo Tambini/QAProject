0346e11dd1bac6f38e55ecbdfcb6cb37eb195ca8
Carolyn Tambini <carolyn@codeandtheory.com>
2018-01-25 19:45:29 -0500

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,175 @@
+function changeImage(theElement) {
+	var clickedName = theElement.id;
+
+	if(clickedName === 'kyle') {
+		if (theElement.style.backgroundImage === 'url("images/Devs/Kyle.png")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Adults/kyle lamble.jpg")';
+            return;
+        }
+        if (theElement.style.backgroundImage === 'url("images/Devs/Adults/kyle lamble.jpg")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Kyle.png")';
+            return;
+        }
+	}
+
+	if (clickedName === 'nate') {
+		if (theElement.style.backgroundImage === 'url("images/Devs/nate_baby_2.jpg")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Adults/Nate.jpg")';
+            return;
+        }
+        if (theElement.style.backgroundImage === 'url("images/Devs/Adults/Nate.jpg")') {
+            theElement.style.backgroundImage = 'url("images/Devs/nate_baby_2.jpg")';
+            return;
+        }
+	}
+
+	if (clickedName === 'aaron') {
+		if (theElement.style.backgroundImage === 'url("images/Devs/aaron.jpg")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Adults/Aaron.jpg")';
+            return;
+        }
+        if (theElement.style.backgroundImage === 'url("images/Devs/Adults/Aaron.jpg")') {
+            theElement.style.backgroundImage = 'url("images/Devs/aaron.jpg")';
+            return;
+        }
+	}
+	if (clickedName === 'chris') {
+		if (theElement.style.backgroundImage === 'url("images/Devs/Chris_Anderson.png")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Adults/Chris Anderson.jpg")';
+            return;
+        }
+        if (theElement.style.backgroundImage === 'url("images/Devs/Adults/Chris Anderson.jpg")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Chris_Anderson.png")';
+            return;
+        }
+	}
+	if (clickedName === 'david') {
+		if (theElement.style.backgroundImage === 'url("images/Devs/David.Mendez.png")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Adults/David Mendez.jpg")';
+            return;
+        }
+        if (theElement.style.backgroundImage === 'url("images/Devs/Adults/David Mendez.jpg")') {
+            theElement.style.backgroundImage = 'url("images/Devs/David.Mendez.png")';
+            return;
+        }
+	}
+	if (clickedName === 'eric') {
+		if (theElement.style.backgroundImage === 'url("images/Devs/Eric.jpeg")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Adults/Eric Chapman.jpg")';
+            return;
+        }
+        if (theElement.style.backgroundImage === 'url("images/Devs/Adults/Eric Chapman.jpg")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Eric.jpeg")';
+            return;
+        }
+	}
+	if (clickedName === 'ewen') {
+		if (theElement.style.backgroundImage === 'url("images/Devs/Ewen.jpg")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Adults/Ewen.jpg")';
+            return;
+        }
+        if (theElement.style.backgroundImage === 'url("images/Devs/Adults/Ewen.jpg")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Ewen.jpg")';
+            return;
+        }
+	}
+	if (clickedName === 'juan') {
+		if (theElement.style.backgroundImage === 'url("images/Devs/Juan.png")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Adults/Juan.jpg")';
+            return;
+        }
+        if (theElement.style.backgroundImage === 'url("images/Devs/Adults/Juan.jpg")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Juan.png")';
+            return;
+        }
+	}
+	if (clickedName === 'kiril') {
+		if (theElement.style.backgroundImage === 'url("images/Devs/Kiril.jpg")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Adults/Kiril.png")';
+            return;
+        }
+        if (theElement.style.backgroundImage === 'url("images/Devs/Adults/Kiril.png")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Kiril.jpg")';
+            return;
+        }
+	}
+	if (clickedName === 'rich') {
+		if (theElement.style.backgroundImage === 'url("images/Devs/Richard Adem.png")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Adults/Richard Adem.jpg")';
+            return;
+        }
+        if (theElement.style.backgroundImage === 'url("images/Devs/Adults/Richard Adem.jpg")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Richard Adem.png")';
+            return;
+        }
+	}
+	if (clickedName === 'shoma') {
+		if (theElement.style.backgroundImage === 'url("images/Devs/Shoma.png")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Adults/Shoma N.jpg")';
+            return;
+        }
+        if (theElement.style.backgroundImage === 'url("images/Devs/Adults/Shoma N.jpg")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Shoma.png")';
+            return;
+        }
+	}
+	if (clickedName === 'walt') {
+		if (theElement.style.backgroundImage === 'url("images/Devs/Walt.jpg")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Adults/WalterM.jpg")';
+            return;
+        }
+        if (theElement.style.backgroundImage === 'url("images/Devs/Adults/WalterM.jpg")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Walt.jpg")';
+            return;
+        }
+	}
+	if (clickedName === 'jenn') {
+		if (theElement.style.backgroundImage === 'url("images/Devs/Jenn.jpg")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Adults/Jenn.jpg")';
+            return;
+        }
+        if (theElement.style.backgroundImage === 'url("images/Devs/Adults/Jenn.jpg")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Jenn.jpg")';
+            return;
+        }
+	}
+	if (clickedName === 'whitely') {
+		if (theElement.style.backgroundImage === 'url("images/Devs/whitely.png")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Adults/whitely.jpg")';
+            return;
+        }
+        if (theElement.style.backgroundImage === 'url("images/Devs/Adults/whitely.jpg")') {
+            theElement.style.backgroundImage = 'url("images/Devs/whitely.png")';
+            return;
+        }
+	}
+	if (clickedName === 'duke') {
+		if (theElement.style.backgroundImage === 'url("images/Devs/duke.jpg")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Adults/Duke.jpg")';
+            return;
+        }
+        if (theElement.style.backgroundImage === 'url("images/Devs/Adults/Duke.jpg")') {
+            theElement.style.backgroundImage = 'url("images/Devs/duke.jpg")';
+            return;
+        }
+	}
+	if (clickedName === 'joseph') {
+		if (theElement.style.backgroundImage === 'url("images/Devs/joseph.png")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Adults/joseph.jpg")';
+            return;
+        }
+        if (theElement.style.backgroundImage === 'url("images/Devs/Adults/joseph.jpg")') {
+            theElement.style.backgroundImage = 'url("images/Devs/joseph.png")';
+            return;
+        }
+	}if (clickedName === 'lacy') {
+		if (theElement.style.backgroundImage === 'url("images/Devs/Lacy.png")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Adults/lacy.jpg")';
+            return;
+        }
+        if (theElement.style.backgroundImage === 'url("images/Devs/Adults/lacy.jpg")') {
+            theElement.style.backgroundImage = 'url("images/Devs/Lacy.png")';
+            return;
+        }
+	}
+}
